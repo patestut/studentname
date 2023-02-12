330196608e43f7402f91a98d5221efa20514c077
patestut <patestut@sheridancollege.ca>
2023-02-12 17:14:10 -0500

    Student Project version 1

--- src/printstudentlife/PrintStudentLife.java
+++ src/printstudentlife/PrintStudentLife.java
@@ -13,17 +13,9 @@ public class PrintStudentLife {
         Student s3 = new Student();
         s3.setName("Heli");
         s3.setAge(18);
-        Student s4 = new Student();
-        s4.setName("Sumandeep");
-        s4.setAge(19);
-        Student s5 = new Student();
-        s5.setName("Anreet");
-        s5.setAge(18);
         list[0] = s1;
         list[1] = s2;
         list[2] = s3;
-        list[3] = s4;
-        list[4] = s5;
         for (int i = 0; i < list.length; i++) {
             System.out.println(list[i].getName() + list[i].getAge());
         }
