e7b50a813132f131af4307e2c6673d9a772fdc24
patestut <122812839+patestut@users.noreply.github.com>
2023-02-12 16:47:10 -0500

    Update PrintStudentLife.java

--- src/printstudentlife/PrintStudentLife.java
+++ src/printstudentlife/PrintStudentLife.java
@@ -1,5 +1,7 @@
 package printstudentlife;
+
 public class PrintStudentLife {
+    
     public static void main(String[] args) {
         Student[] list = new Student[3];
         Student s1 = new Student();
@@ -14,10 +16,14 @@ public class PrintStudentLife {
         Student s4 = new Student();
         s4.setName("Sumandeep");
         s4.setAge(19);
+        Studnet s5 = new Student();
+        s5.setName("Anreet");
+        s5.setAge(18);
         list[0] = s1;
         list[1] = s2;
         list[2] = s3;
         list[3] = s4;
+        list[4] = s5;
         for (int i = 0; i < list.length; i++) {
             System.out.println(list[i].getName() + list[i].getAge());
         }
