44a5ed3fcbfef1d498ba61c962566bc05d113c4f
patestut <patestut@sheridancollege.ca>
2023-02-12 19:23:34 -0500

    Student Project version 1

---
+++ src/printstudentlife/PartTimeStudent.java
@@ -0,0 +1,13 @@
+package printstudentlife;
+
+/**
+ *
+ * @author spate
+ */
+public class PartTimeStudent extends Student{
+    private int num;
+    public PartTimeStudent(String name, int num){
+        
+        this.num = num;
+    }
+}
